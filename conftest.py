1c2328481103d3bfdbffea01761c2bd5aff66f3b
Djon <lioni767@yandex.ru>
2026-05-28 00:16:19 +0300

    move API_BASE_URL to .env

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
+import os
 import pytest
+from dotenv import load_dotenv
 from api.client import ApiClient
+
+load_dotenv()
 
 
 @pytest.fixture(scope='session')
 def api_client():
-    base_url = 'https://fakestoreapi.com'
+    base_url = os.getenv('API_BASE_URL', 'https://fakestoreapi.com')
     return ApiClient(base_url)
