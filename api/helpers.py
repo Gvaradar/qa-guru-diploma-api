6be1a58604b6aa265b904937465e1748bfc35348
Djon <lioni767@yandex.ru>
2026-05-28 03:57:27 +0300

    return assert

--- api/helpers.py
+++ api/helpers.py
@@ -1,3 +1,2 @@
 def assert_status_code(response, expected_code=200):
-    print(f"DEBUG: actual status = {response.status_code}, expected = {expected_code}")
-    # assert response.status_code == expected_code  # временно отключаем
+    assert response.status_code == expected_code
